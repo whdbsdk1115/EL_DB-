69f93821cffa153d92723e0c712cb0cdd1e315fc
kghyun <86757153+kghyun@users.noreply.github.com>
2021-07-11 18:18:49 +0900

    Update ScoreInsert.cs

--- ScoreInsert.cs
+++ ScoreInsert.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
 //게임 종료 후, isnert하는 cs
+public class ScoreInsert : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+    
+    }
+    
+}
