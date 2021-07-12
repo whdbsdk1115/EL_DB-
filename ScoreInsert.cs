b1654bbcb43cd116c5cc1daa1f25abe065b09da4
Leeks1997 <58100311+Leeks1997@users.noreply.github.com>
2021-07-12 15:12:00 +0900

    Update ScoreInsert.cs

--- ScoreInsert.cs
+++ ScoreInsert.cs
@@ -4,17 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//게임 종료 후, isnert하는 cs
+//게임 종료 후, insert하는 cs
 public class ScoreInsert : MonoBehaviour
 {
-
-    void Start()
+    dbconn mysqlDB = new dbconn();
+    public void OnClickUpdate()
     {
-
-    }
-    void Update()
-    {
-    
+        mysqlDB.sqlcmdall("INSERT INTO `player`(`musicnum`, `username`, `score`) VALUES (10,'LEE',100000)");
     }
     
 }
+
+//스크립트 파일 button에 적용하고 On Click()부분에 맞는 함수 적용시켜주면 돼용
