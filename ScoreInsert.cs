e2ea029c0817cbc1e270ac63f9b525a7e44b1c53
Leeks1997 <58100311+Leeks1997@users.noreply.github.com>
2021-07-12 17:51:20 +0900

    Update ScoreInsert.cs

--- ScoreInsert.cs
+++ ScoreInsert.cs
@@ -12,7 +12,11 @@ public class ScoreInsert : MonoBehaviour
     {
         mysqlDB.sqlcmdall("INSERT INTO `player`(`musicnum`, `username`, `score`) VALUES (10,'LEE',100000)");
     }
-    
+    public void RetryGame()
+    {
+        Application.Quit(); //게임종료 및 재시작 버튼 적용 함수
+        Debug.Log("게임 재시작");
+    }
 }
 
 //스크립트 파일 button에 적용하고 On Click()부분에 맞는 함수 적용시켜주면 돼용
